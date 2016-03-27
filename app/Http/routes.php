78a9e7e94d5f2746c34a0b016ff9e5752c14471f
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-03-26 22:44:13 -0500

    Creatd routes with '/api' prefix group as well as 'auth' and 'web' middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,12 +11,38 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Everything in this group has access to the CSRF Token (web middleware has access to this, as shown in Kernel.php)
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
     Route::get('/home', 'HomeController@index');
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    // Everything in this group will have a url that starts with '/api'
+    Route::group(['prefix' => 'api'], function() {
+
+        Route::resource('users', 'UsersController', [
+            'only' => ['index', 'show']
+        ]);
+
+        Route::resource('posts', 'PostsController', [
+            'only' => ['index', 'show']
+        ]);
+
+        // Everything in this group will only be accessible once logged in
+        Route::group(['middleware' => 'auth'], function() {
+
+            // No store function for users because that feature is prebuilt
+            Route::resource('users', 'UsersController', [
+                'only' => ['update', 'destroy']
+            ]);
+
+            Route::resource('posts', 'PostsController', [
+                'only' => ['store', 'update', 'destroy']
+            ]);
+        });
+    });
 });
