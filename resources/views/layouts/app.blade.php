7f005cd98a9367859586297a076f8b1f0cdab3a1
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-04-14 00:39:04 -0500

    Further updated Laravel nav formatting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,12 +25,6 @@
         .fa-btn {
             margin-right: 6px;
         }
-
-        .logo {
-            font-size: 50px;
-            width: 100%;
-            color: #3592b2;
-        }
         #all-posts {
             height: 400px;
             overflow: scroll;
@@ -58,6 +52,9 @@
             text-decoration: none;
         }
 
+        .logo {
+            font-size: 40px;
+        }
         .title {
             background-color: lightseagreen;
             color: white;
@@ -144,14 +141,14 @@
 
                 <!-- Collapsed Hamburger -->
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="sr-only">toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand logo" href="{{ url('/') }}">
                     b.itter
                 </a>
             </div>
@@ -159,15 +156,15 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ url('/home') }}">home</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        <li><a href="{{ url('/login') }}">login</a></li>
+                        <li><a href="{{ url('/register') }}">register</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -175,7 +172,7 @@
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>logout</a></li>
                             </ul>
                         </li>
                     @endif
