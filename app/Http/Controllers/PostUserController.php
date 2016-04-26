361e707a13d0e639bf7168fc2b8c73adf0007e53
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-04-25 20:42:36 -0500

    Updated PostUserController's destroy() method to use manual DB command

--- app/Http/Controllers/PostUserController.php
+++ app/Http/Controllers/PostUserController.php
@@ -33,6 +33,15 @@ class PostUserController extends Controller
      */
     public function destroy($id)
     {
-        //
+
+        $user = \Auth::user();
+        $like = $user->likes()->where(['post_user.user_id' => \Auth::user()->id, 'post_user.post_id' => $id])->get();
+
+//        $user->likes()->detach($like->id);
+//        $like->delete();
+
+        \DB::delete('delete from post_user where user_id=? and post_id=?', array($user->id, $id));
+
+        return $like;
     }
 }
