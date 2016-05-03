1a683490c54ff63bb38ef97a0c2efac2029f2121
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-05-02 22:16:24 -0500

    Updated app name on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <span style="font-size: 190px; color: #3592b2;">b.itter</span>
+            <span style="font-size: 190px; color: #3592b2;">b.it</span>
             <h1>&nbsp;the basic twitter</h1>
         </div>
     </div>
