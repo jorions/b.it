2d853e339cef034747f08d10058b9e9ae46e87a2
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-05-08 18:51:35 -0500

    Created .css file for styling and updated favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,167 +2,24 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/ico" />
 
     <title>b.it</title>
 
     <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css' />
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css' />
 
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" rel="stylesheet" type="text/css" />
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('styles.css') }}" rel="stylesheet" />
 
-    <style>
-        /*body {*/
-            /*font-family: 'Lato';*/
-        /*}*/
-
-        .fa-btn {
-            margin-right: 6px;
-        }
-        #all-posts {
-            height: 400px;
-            overflow-x: hidden;
-            overflow-y: scroll;
-        }
-        #main-window {
-            height: 442px;
-            overflow-x: hidden;
-            overflow-y: scroll;
-        }
-
-        .post-list-container {
-            overflow: hidden;
-        }
-
-        .post-container {
-            border-bottom: 1px dotted darkgray;
-            line-height: 15px;
-            padding: 7px;
-            border-bottom-left-radius: 8px;
-            border-bottom-right-radius: 8px;
-        }
-        a, a:hover, a:active, a:visited, a:link {
-            text-decoration: none;
-        }
-
-        .logo {
-            font-size: 40px;
-            color: #3592b2;
-        }
-        .post {
-            width: 70%;
-            display: inline-block;
-        }
-        .heart {
-            font-size: 60px;
-            width: 20%;
-            display: inline-block;
-            text-align: right;
-            padding-top: 10px;
-        }
-        #heart-normal {
-            color: #efe9ec;
-        }
-        #heart-normal:hover {
-            color: #d6d1d3;
-        }
-        #heart-favorited {
-            color: rgba(225, 57, 60, 0.81);
-        }
-        #heart-favorited:hover {
-            color: rgba(205, 52, 55, 0.81);
-        }
-        .title {
-            background-color: lightseagreen;
-            color: white;
-            font-size: 20px;
-            text-align: center;
-            border-top-left-radius: 8px;
-            border-top-right-radius: 8px;
-        }
-        .title-favorites {
-            background-color: #3592b2;
-            color: white;
-            font-size: 20px;
-            text-align: center;
-        }
-        .title-post-viewer {
-            background-color: #d6d1d3;
-            color: white;
-            font-size: 20px;
-            height: 32px;
-            text-align: center;
-            overflow: hidden;
-        }
-        .post-viewer-details {
-            text-align: center;
-            color: #3592b2;
-            overflow: hidden;
-            height: 76px;
-
-        }
-
-        #error {
-            background-color: rgba(205, 52, 55, 0.81);
-            color: white;
-            font-size: 20px;
-            text-align: center;
-        }
-
-        input[type="text"] {
-            width: 100%;
-            margin-bottom: 0px;
-            border-radius: 0;
-        }
-
-        #submit {
-            margin-bottom: 0px;
-            width: 100%;
-            background-color: #3592b2;
-            color: white;
-            height: 38px;
-            font-size: 20px;
-            line-height: 33px;
-            text-align: center;
-            border-top-right-radius: 0;
-            border-top-left-radius: 0;
-
-            /*Removed for formatting consistency*/
-            /*border-bottom-left-radius: 9px;*/
-            /*border-bottom-right-radius: 9px;*/
-        }
-        #submit:hover {
-            border: 1px solid gray;
-        }
-        #submit:active {
-            background-color: #2e7c98;
-            color: #d5d5d5;
-        }
-
-        #content-container {
-            margin-bottom: 40px;
-            border: 1px solid darkgray;
-
-            /*Removed for formatting consistency*/
-            /*border-radius: 10px;*/
-            border-top-right-radius: 10px;
-            border-top-left-radius: 10px;
-        }
-        #post-viewer-container {
-            height: 110px;
-        }
-        span {
-            font-size: 12px;
-            color: #d6d1d3;
-        }
-
-    </style>
 </head>
 <body id="app-layout">
     <nav class="navbar navbar-default navbar-static-top">
